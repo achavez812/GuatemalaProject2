6bada32d113d6fe7ca1d2cba0468e35487748d95
Alejandro Chavez <achavez812@gmail.com>
2014-06-30 13:23:09 -0700

    added basic fields for child visit

--- GuateMedic-master/GuateMedic/src/com/stanford/guatemedic/AddNewChildVisitActivity.java
+++ GuateMedic-master/GuateMedic/src/com/stanford/guatemedic/AddNewChildVisitActivity.java
@@ -14,6 +14,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class AddNewChildVisitActivity extends ActionBarActivity{
+	
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -69,14 +70,16 @@ public class AddNewChildVisitActivity extends ActionBarActivity{
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		//getMenuInflater().inflateR.men.main, menu);
+		getMenuInflater().inflate(R.menu.add_child_visit_menu, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_addchildvisit_submit) {
+			
+			Toast.makeText(getApplication(), "CHECK", Toast.LENGTH_LONG).show();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -99,7 +102,8 @@ public class AddNewChildVisitActivity extends ActionBarActivity{
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_add_new_child_visit1, container, false);
 			final String child_id = getArguments().getString("child_id");
-
+			
+			
 			return rootView;
 		}
 	}
@@ -121,7 +125,7 @@ public class AddNewChildVisitActivity extends ActionBarActivity{
 		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_add_new_child_visit2, container, false);
 			final String child_id = getArguments().getString("child_id");
-
+			
 			return rootView;
 		}
 	}
