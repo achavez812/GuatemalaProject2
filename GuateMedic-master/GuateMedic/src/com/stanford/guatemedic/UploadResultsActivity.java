c0f93962119d11a02133199fb52f18b37ca6efa9
Aditya Aggarwal <adityaaggarwal@Anils-MacBook-Pro.local>
2014-07-01 17:15:55 -0700

    Layout improvements to add child

--- GuateMedic-master/GuateMedic/src/com/stanford/guatemedic/UploadResultsActivity.java
+++ GuateMedic-master/GuateMedic/src/com/stanford/guatemedic/UploadResultsActivity.java
@@ -12,7 +12,7 @@ public class UploadResultsActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_upload_results);
+		setContentView(R.layout.activity_upload_login);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
